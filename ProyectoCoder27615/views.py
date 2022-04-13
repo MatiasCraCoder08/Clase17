339f7556afac09ad1cc6cc8f0c29bdee6f8f325f
MatiasCra <cravchikm@gmail.com>
2022-04-12 23:14:12 -0300

    mal import y BASEDIR

--- ProyectoCoder27615/views.py
+++ ProyectoCoder27615/views.py
@@ -1,7 +1,8 @@
-from contextvars import Context
 from django.http import HttpResponse
 from datetime import datetime
 from django.template import Template, Context
+# Importo settings para poder usar una variable del archivo settings.py
+from django.conf import settings
 
 
 def saludo(request):
@@ -25,9 +26,19 @@ def anio_nacimiento(request, edad):
 
 
 def con_plantilla(request):
-    mi_html = open("/home/matiascra/Documents/Coderhouse/Curso 08-02-2022/Clase17/ProyectoCoder27615/ProyectoCoder27615/plantillas/plantilla.html")
+    # Con ruta absoluta al archivo .html
+    # mi_html = open("/home/matiascra/Documents/Coderhouse/Curso 08-02-2022/Clase17/ProyectoCoder27615/ProyectoCoder27615/plantillas/plantilla.html")
+    
+    # Uso una variable del archivo settings.py que tiene la ruta al proyecto. No
+    # es exactamente un string y puedo concatenarle texto con la "/"
+    mi_html = open(settings.BASE_DIR / "ProyectoCoder27615/plantillas/plantilla.html")
+    # Creo una plantilla con los datos del html
     plantilla = Template(mi_html.read())
     mi_html.close()
+
+    # Necesito un "contexto" para poder renderizar una plantilla a algo que se
+    # pueda mostrar.
     mi_contexto = Context(plantilla)
     documento = plantilla.render(mi_contexto)
+
     return HttpResponse(documento)
